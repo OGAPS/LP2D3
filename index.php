a5d0c937bc25d35466da9a9f2a3b05053e39b7ab
OGAPS <101257900+OGAPS@users.noreply.github.com>
2023-08-04 21:33:30 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,53 +1,68 @@
 <?php
-class fruta{
-    //atributos da classe
-    public $nome;
+require ("Carro.php");
+class Fruta{
+    // atributos da classe
+    private $nome;
     public $cor;
 
-    //metodos
+    //Métodos
     function __construct($nome, $cor){
-            $this->nome = $nome;
-            $this->cor = $cor;
-    }
-
-
-    function set_nome($nome){
-        if($nome == "Gabriel"){
-            $this->nome = "invalido";
-        }else{
         $this->nome = $nome;
+        $this->cor = $cor;
     }
-}
-
+//encapsulamento...
+    function set_nome($nome){
+        if ($nome == "Gabriel"){
+            $this->nome = "inválido";
+        }else{
+            $this->nome = $nome;
+        }
+        
+    }
     function set_cor($cor){
         $this->cor = $cor;
     }
-
     function get_nome(){
         return $this->nome;
     }
-
     function get_cor(){
         return $this->cor;
     }
-    
+
 }
-//Criação de um objeto do tipo maça      new -> serve para criar um objeto
-$maca = new Fruta("maca","vermelho");
-$banana = new Fruta("banana", "amarelo");
+// Criação de um objeto do tipo Fruta
+$maca = new Fruta("maçã","vermelha");
+$banana = new Fruta("banana","amarela");
 $mamao = new Fruta("mamão", "laranja");
 
-//Imprimir na tela o nome e a cor dos objetos
+// Criação de um objeto do tipo Carro
+$carro1 = new Carro("Hyndai","hb20",2015);
+echo "<br> Carro 1 - marca = ".     
+        $carro1->get_marca().
+     "<br> modelo = ".
+        $carro1->get_modelo().
+     "<br> ano = ".
+        $carro1->get_ano();
 
+$carro2 = new Carro("VolksWaggen","fusca",1974);
+echo "<br> Carro 2 - marca = ".     
+        $carro2->get_marca().
+     "<br> modelo = ".
+        $carro2->get_modelo().
+     "<br> ano = ".
+        $carro2->get_ano();
+
+// Imprimir na tela o nome e a cor dos
+//objetos
 //$maca->cor = "vermelho";
-//$maca->nome = "maçâ";
-//$maca->set.nome("maçã");
+//$maca->nome = "maçã";
+//$maca->set_nome("Renato");
 echo "<br>".$maca->get_nome();
 echo "<br>".$maca->get_cor();
 
 //$banana->cor = "amarela";
 //$banana->nome = "banana prata";
-//$banana->set.nome ("banana prata");
+//$banana->set_nome("banana prata");
 echo "<br>".$banana->get_nome();
 echo "<br>".$banana->get_cor();
 
